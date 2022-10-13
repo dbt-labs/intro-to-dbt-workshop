8541e6af2e754bab24df773e9c13b3848a319f44
MichelleBaird-DBT <108753201+MichelleBaird-DBT@users.noreply.github.com>
2022-10-13 09:40:52 -0400

    Update dim_customers.sql

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,6 +6,7 @@ nations as (
 
     select * from {{ ref('stg_sample_data__nations') }}    
 ),
+
 regions as (
 
     select * from {{ ref('stg_sample_data__regions') }}
@@ -18,8 +19,7 @@ final as (
         customers.customer_phone,
         customers.customer_acct_balance,
         nations.nation_key as nation_key,
-        regions.region_key as region_key,
-        regions.region_name as region
+        regions.region_key as region_key
         -- add nation name 
         -- add region name 
 
