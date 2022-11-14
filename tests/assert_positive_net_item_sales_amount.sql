f6400c7fc8e6b7452ac96a42e6bb350cf6008328
MichelleBaird-DBT <108753201+MichelleBaird-DBT@users.noreply.github.com>
2022-11-14 12:07:03 -0500

    Delete assert_positive_net_item_sales_amount.sql
    singular test isn't supposed to be added til the testing section

--- tests/assert_positive_net_item_sales_amount.sql
+++
@@ -1,8 +0,0 @@
--- Refunds have a negative amount, so the total amount should always be >= 0.
--- Therefore return records where this isn't true to make the test fail.
-
-select
-    order_key,
-    net_item_sales_amount
-from {{ ref('fct_orders') }}
-having not(net_item_sales_amount>= 0)
